6f5066a5236a30dfe6dfe915b6a9320ebf34fa47
Frank Brammer <fbrammer@brammeris.com>
2018-04-12 11:18:09 -0400

    Initial Commit.  Just started menu.

---
+++ WiFi Auto Reset/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WiFi Auto Reset
+//
+//  Created by Frank Brammer on 4/12/18.
+//  Copyright © 2018 BrammerIS. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
